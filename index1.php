e7f96eee3f5fa1c9f5927ef91308c08cf940b50d
AlfredMelwinAZ <165752794+AlfredMelwinAZ@users.noreply.github.com>
2024-04-02 06:47:59 +0000

    Update index1.php

--- index1.php
+++ index1.php
@@ -1,12 +1,9 @@
 <!DOCTYPE html>
 <html>
 <body>
-
+ 
 <?php
-$color = "Yellow";
-echo "My Bag is " . $color . "<br>";
-echo "My Bike is " . $COLOR . "<br>";
-echo "My Book is " . $coLOR . "<br>";
+echo "My first PHP script!";
 ?>
 
 </body>
