761bb9bd77d8fc8e2df4b488c6cc10d18f8b95e3
Alfred <alfredmelwinAZ@outlook.com>
2024-04-02 05:57:45 +0000

    initial commit

---
+++ index1.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$color = "Yellow";
+echo "My Bag is " . $color . "<br>";
+echo "My Bike is " . $COLOR . "<br>";
+echo "My Book is " . $coLOR . "<br>";
+?>
+
+</body>
+</html>
